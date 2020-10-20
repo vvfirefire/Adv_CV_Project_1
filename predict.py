e98050874867ad36a0ab6e0a4a938ce00eed116d
vv <zhxu6915@uni.sydney.edu.au>
2020-10-20 17:14:18 +0800

    Push

---
+++ predict.py
@@ -0,0 +1,208 @@
+from __future__ import print_function
+
+import argparse
+import os
+import shutil
+import time
+import random
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.distributed as dist
+import torch.optim
+import torch.utils.data
+import torch.utils.data.distributed
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision
+import models
+from loss import FocalLoss
+from math import cos, pi
+
+from celeba import CelebA, ReadPrivateSetCelebA
+from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig
+from tensorboardX import SummaryWriter
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+# Parse arguments
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('-d', '--data', default='', type=str)
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet50',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet50)')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+# Optimization options
+parser.add_argument('--epochs', default=30, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--train-batch', default=150, type=int, metavar='N',
+                    help='train batchsize (default: 256)')
+parser.add_argument('--test-batch', default=100, type=int, metavar='N',
+                    help='test batchsize (default: 200)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--lr-decay', type=str, default='step',
+                    help='mode for learning rate decay')
+parser.add_argument('--step', type=int, default=30,
+                    help='interval for learning rate decay in step mode')
+parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
+                    help='decrease learning rate at these epochs.')
+parser.add_argument('--turning-point', type=int, default=100,
+                    help='epoch number from linear to exponential decay mode')
+parser.add_argument('--gamma', type=float, default=0.1,
+                    help='LR is multiplied by gamma on schedule.')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+# Checkpoints
+parser.add_argument('-c', '--checkpoint', default='checkpoints', type=str, metavar='PATH',
+                    help='path to save checkpoint (default: checkpoints)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+# Architecture
+parser.add_argument('--cardinality', type=int, default=32, help='ResNeXt model cardinality (group).')
+parser.add_argument('--base-width', type=int, default=4, help='ResNeXt model base width (number of channels in each group).')
+parser.add_argument('--groups', type=int, default=3, help='ShuffleNet model groups')
+# Miscs
+parser.add_argument('--manual-seed', type=int, help='manual seed')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--world-size', default=1, type=int,
+                    help='number of distributed processes')
+parser.add_argument('--dist-url', default='tcp://224.66.41.62:23456', type=str,
+                    help='url used to set up distributed training')
+parser.add_argument('--dist-backend', default='gloo', type=str,
+                    help='distributed backend')
+# Device options
+parser.add_argument('--gpu-id', default='0', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+
+    args.distributed = args.world_size > 1
+
+    if args.distributed:
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url,
+                                world_size=args.world_size)
+    # Use CUDA
+    os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu_id
+    use_cuda = torch.cuda.is_available()
+
+    # Random seed
+    if args.manual_seed is None:
+        args.manual_seed = random.randint(1, 10000)
+    random.seed(args.manual_seed)
+    torch.manual_seed(args.manual_seed)
+    if use_cuda:
+        torch.cuda.manual_seed_all(args.manual_seed)
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        model = models.__dict__[args.arch](pretrained=True)
+    elif args.arch.startswith('resnext'):
+        model = models.__dict__[args.arch](
+                    baseWidth=args.base_width,
+                    cardinality=args.cardinality,
+                )
+    elif args.arch.startswith('shufflenet'):
+        model = models.__dict__[args.arch](
+                    groups=args.groups
+                )
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        model = models.__dict__[args.arch]()
+
+    if not args.distributed:
+        if args.arch.startswith('alexnet') or args.arch.startswith('vgg'):
+            model.features = torch.nn.DataParallel(model.features)
+            model.cuda()
+        else:
+            model = torch.nn.DataParallel(model).cuda()
+    else:
+        model.cuda()
+        model = torch.nn.parallel.DistributedDataParallel(model)
+
+    # define loss function (criterion) and optimizer
+    # criterion = nn.CrossEntropyLoss().cuda()
+    criterion = FocalLoss()
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    # optionally resume from a checkpoint
+    title = 'CelebA-' + args.arch
+    if not os.path.isdir(args.checkpoint):
+        mkdir_p(args.checkpoint)
+
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+            args.checkpoint = os.path.dirname(args.resume)
+            logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title, resume=True)
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+    else:
+        logger = Logger(os.path.join(args.checkpoint, 'log.txt'), title=title)
+        logger.set_names(['Learning Rate', 'Train Loss', 'Valid Loss', 'Train Acc.', 'Valid Acc.'])
+
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    predict_dataset = ReadPrivateSetCelebA(args.data, 'testset.txt', transforms.Compose([
+            transforms.ToTensor(),
+            normalize,
+        ]))
+
+    private_loader = torch.utils.data.DataLoader(
+        predict_dataset,
+        shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    checkpoint = torch.load(os.path.join(args.checkpoint, 'model_best.pth.tar'))
+    model.load_state_dict(checkpoint['state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer'])
+
+    output_file = open ('prediction.txt','w')
+    model.eval()
+
+    for i, (input, path) in enumerate (private_loader):
+        ph = path[0][13:]
+        output_file.write (ph + " ")
+        output = model(input)
+        for j in range(len(output)):
+          index = torch.max(output[j], dim=1)[1]
+          index = index.cpu().numpy()
+          if index == 0:
+            output_file.write ("-1 ")
+          else:
+            output_file.write ("1 ")
+        output_file.write ("\n")
+    output_file.close()
+if __name__ == '__main__':
+    main()
